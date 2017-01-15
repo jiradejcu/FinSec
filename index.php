a9db8f7612b7351d5403928d51e7c9cadfe725cf
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2017-01-15 14:05:13 +0700

    try echo bot

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('x');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'x']);
+define('CHANNEL_ACCESS_TOKEN', '15o60jaZ2VS5zxNCn/YHfie4/JZBYA9GbpnbmFCiaQkt91rHH1/iD6s6Y7g4h2KbzAcuDCj8cNd3JeNuAVigoEwZ/vhfwQ4udINHfI7p1iC+n1e+/lJEWA59NYGE8YGPeB4TiEq12Oz3SXtbpanm8QdB04t89/1O/w1cDnyilFU=');
+define('CHANNEL_SECRET', '60f172d664490d49d7ff28b802949468');
 
-$response = $bot->replyText('x', 'hello!');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
+
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+
+if (!empty($events['events'])) {
+	foreach ($events['events'] as $event) {
+		switch ($event['type']) {
+			case 'message':
+				$message = $event['message'];
+				switch ($message['type']) {
+					case 'text':
+						$bot->replyText($event['replyToken'], $message['text']);
+						break;
+					default:
+						error_log("Unsupported message type: " . $message['type']);
+						break;
+				}
+				break;
+			default:
+				error_log("Unsupported event type: " . $event['type']);
+				break;
+		}
+	}
+}
