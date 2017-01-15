0cdcf39f3839a5ca626c0669c44c171715ba6932
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2017-01-15 13:47:49 +0700

    try ini bot using sdk

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('x');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'x']);
+
+$response = $bot->replyText('x', 'hello!');
